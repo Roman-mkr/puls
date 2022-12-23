20d1b370abe2dcb285b8056506fef51c8d22851f
Roman <samohvalov.r.v@gmail.com>
2022-12-23 16:59:57 +0400

    added phpmailer

--- src/js/script.js
+++ src/js/script.js
@@ -149,4 +149,22 @@ $(document).ready(function() {
 	validateForms('#modal_consult form');
 
 /* 	$('input[name=phone]').mask("+7 (999) 999-99-99"); */
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+		if (!$(this).valid()) {
+			return;
+		}
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#modal_order, #modal_consult').fadeOut();
+			$('.overlay, #modal_feedback').fadeIn();
+			$('form').trigger('reset');
+		});
+		return false;
+	});
 });
